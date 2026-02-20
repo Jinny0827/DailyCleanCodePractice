f54c6d0de1ca57980e41c3abaaa6320ffdde077e
Wonjin <Wonjin@DESKTOP-GKOEREA>
2026-02-20 09:55:36 +0900

    260220 정원진
    Day 78 — 함수형 에러 처리 (Vavr)
    
    1. Either를 통한 Try 예외 캡처
    2. Either로 성공/실패 분리 (mapLeft)
    3. flatMap으로 안전하게 연결(메서드 체이닝)
    4. fold() 로 최종 결과 처리

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.google.guava:guava:33.1.0-jre")
+    implementation ("io.vavr:vavr:0.10.4")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
 
 tasks.test {
